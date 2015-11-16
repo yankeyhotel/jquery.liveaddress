8805c0b45d0ae73ae1d5ad2eb80867b31316846e
camiblanch <camiblanch@users.noreply.github.com>
2015-11-16 10:18:07 -0700

    Uncommented fix

--- src/jquery.liveaddress.js
+++ src/jquery.liveaddress.js
@@ -1008,7 +1008,7 @@
 						$(domfields['state']).val(suggestion.state).change();
 				}
 				if (domfields['city']) {
-					// $(domfields['city']).val("").change()
+					$(domfields['city']).val("").change()
 					addr.usedAutocomplete = true;
 					$(domfields['city']).val(suggestion.city).change();
 				}
