36d6347ca7317ab804086bde1e62daaf8ba944cd
camiblanch <camiblanch@users.noreply.github.com>
2016-03-29 14:47:40 -0600

    Updated customizations

--- customizations/bigcommerce.js
+++ customizations/bigcommerce.js
@@ -1,53 +1,49 @@
 /*
-	Adapter for BigCommerce. The forms on BigCommerce are nearly imcomprehensible and poorly
-	marked up, so automapping fails. This adapter attempts to automap fields specifically
-	for BigCommerce sites.
+ Adapter for BigCommerce. The forms on BigCommerce are nearly imcomprehensible and poorly
+ marked up, so automapping fails. This adapter attempts to automap fields specifically
+ for BigCommerce sites.
 
-	Tested on BigCommerce v7408.
-	
-	Assuming:
-	var liveaddress = $.LiveAddress(...);
+ Tested on BigCommerce v7408.
 
-	To use:
-	1) Initialize with autoMap: false
-	2) Map fields when document is ready, like so:
-		$(function() {
-			liveaddress.mapFields(BigCommerceAdapter.map());
-		});
-*/
+ Assuming:
+ var liveaddress = $.LiveAddress(...);
+
+ To use:
+ 1) Initialize with autoMap: false
+ 2) Map fields when document is ready, like so:
+ $(function() {
+ liveaddress.mapFields(BigCommerceAdapter.map());
+ });
+ */
 
 var BigCommerceAdapter = {
-	map: function()
-	{
-		var getMap = function()
-		{
+	map: function () {
+		var getMap = function () {
 			var map = {};	// This will be the finished map
 			var translation = {
-				addressline1: 'street',		// BigCommerce values on the left (lowercased); LiveAddress names on the right
-				addressline2: 'street2',
-				city: 'city',
-				state: 'state',
-				zip: 'zipcode',
+				addressline1: 'address1',		// BigCommerce values on the left (lowercased); LiveAddress names on the right
+				addressline2: 'address2',
+				city: 'locality',
+				state: 'administrative_area',
+				zip: 'postal_code',
 				country: 'country'
 			};
 
 			var fieldIdentifierSelector = 'form input.FormFieldPrivateId[type=hidden]';	// Selector for hidden fields to identify the visible fields
 			var visibleFieldSelector = 'select.FormField, input.FormField';				// Selects the visible field following the identifying field(s)
 
-			jQuery(fieldIdentifierSelector).filter(function() {
+			jQuery(fieldIdentifierSelector).filter(function () {
 				return jQuery(this).nextAll(visibleFieldSelector).is(':visible');	// Field must be visible
-			}).each(function(i)
-			{
+			}).each(function (i) {
 				var liveAddressInputFieldName = translation[jQuery(this).val().toLowerCase()];
-				if (liveAddressInputFieldName && !map[liveAddressInputFieldName])
-				{
+				if (liveAddressInputFieldName && !map[liveAddressInputFieldName]) {
 					var fieldID = jQuery(this).nextAll('select.FormField, input.FormField').attr('id');
 					map[liveAddressInputFieldName] = '#' + fieldID;
 				}
 			});
 
 			return map;
-		}
+		};
 
 		if (jQuery.isReady)
 			return getMap();
--- customizations/get-coordinates.js
+++ customizations/get-coordinates.js
@@ -1,12 +1,11 @@
 /*
-	Get latitude/longitude for a verified address.
-	
-	Assuming:
-	var liveaddress = $.LiveAddress(...);
-*/
+ Get latitude/longitude for a verified address.
 
-liveaddress.on("AddressAccepted", function(event, data, previousHandler)
-{
+ Assuming:
+ var liveaddress = $.LiveAddress(...);
+ */
+
+liveaddress.on("AddressAccepted", function (event, data, previousHandler) {
 	if (data.response.chosen)
 		console.log(data.response.chosen.metadata.latitude, data.response.chosen.metadata.longitude);
 	previousHandler(event, data);
